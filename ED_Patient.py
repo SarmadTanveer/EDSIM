6233972c095baa7e98978ff164523d28ac82fe39
Sarmad <tanveer.sarmad@gmail.com>
2022-01-26 05:11:10 -0500

    Fixed patient class

--- ED_Patient.py
+++ ED_Patient.py
@@ -1,12 +1,13 @@
+from os import sep
 import random
 
 #ctas _dist should have the from: {1:0.2, 2: 0.3, 3: 0.1, 4:0.1, 5: 0.3}
 class Patient:
-    p_tally= 0
+    p_id= 0
     def __init__(self, ctas_dist):
-        self.id = self.p_tally
+        self.id = self.p_id
         self.CTAS_Level = self.setCTAS(ctas_dist)
-        self.p_id += self.p_tally
+        Patient.p_id += 1
 
     #Output a CTAS level depending on ctas distribution provided by user
     def setCTAS(self, ctas_dist):
@@ -22,9 +23,9 @@ class Patient:
         #sample>0.2 and sample <=0.5 = 30 % chance
         elif(sample > ctas_dist[1] and sample<= (ctas_dist[1] + ctas_dist[2]) ):
             ctas_level = 2
-        elif(sample> (ctas_dist[1] + ctas_dist[2]) and sample <= (ctas_dist[1] + ctas_dist[2] and ctas_dist[3])): 
+        elif(sample> (ctas_dist[1] + ctas_dist[2]) and sample <= (ctas_dist[1] + ctas_dist[2] + ctas_dist[3])): 
             ctas_level = 3
-        elif(sample>(ctas_dist[1]+ctas_dist[2]+ ctas_dist[3]) and sample<=(ctas_dist[1]+ctas_dist[2] +ctas_dist[3] and ctas_dist[4])):
+        elif(sample>(ctas_dist[1]+ctas_dist[2]+ ctas_dist[3]) and sample<=(ctas_dist[1]+ctas_dist[2] +ctas_dist[3] + ctas_dist[4])):
             ctas_level = 4
         
         return ctas_level
@@ -40,4 +41,15 @@ class walkInPatient(Patient):
 class ambulancePatient(Patient):
     def __init__(self, ctas_dist):
         # call super
-        super.__init__(ctas_dist)
+        super().__init__(ctas_dist)
+
+#Pateint Test 
+ctas_dist = {1:0.2, 2: 0.3, 3: 0.1, 4:0.1, 5: 0.3}
+
+for i in range (0,100):
+    wp = Patient(ctas_dist)
+    print("CTAS level for walkin pateint ", wp.id, " : ", wp.CTAS_Level, sep = "")
+
+for i in range(0,100):
+    ap = ambulancePatient(ctas_dist)
+    print("CTAS level for ambulance patient ", ap.id, " : ",ap.CTAS_Level, sep="")
